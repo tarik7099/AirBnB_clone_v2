fd267f963013f3aa4443db1ff83c3495a3b3382b
tarikkhadri <tarikkhadri11@gmail.com>
2024-03-19 22:54:33 +0000

    s

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,8 @@
+-- Creates a MySQL server with:
+--   Database hbnb_dev_db.
+--   User hbnb_dev with password hbnb_dev_pwd in localhost.
+--   Grants all privileges for hbnb_dev on hbnb_dev_db.
+--   Grants SELECT privilege for hbnb_dev on performance_schema.
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER
