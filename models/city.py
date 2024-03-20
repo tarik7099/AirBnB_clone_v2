394777b89a6de64a3c8071fe6c62c686c3afc0a9
tarikkhadri <tarikkhadri11@gmail.com>
2024-03-20 01:13:07 +0000

    f

--- models/city.py
+++ models/city.py
@@ -1,9 +1,8 @@
-#!/usr/bin/python3
-""" City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
+class City(BaseModel, Base):
+    __tablename__ = 'cities'
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    places = relationship("Place", cascade="all, delete", back_populates="city")
--- models/user.py
+++ models/user.py
@@ -1,11 +1,8 @@
-#!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
+class User(BaseModel, Base):
+    __tablename__ = 'users'
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    places = relationship("Place", cascade="all, delete", back_populates="user")
