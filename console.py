778129b025b24f80d839e8ad53711c06bdf110af
tarikkhadri <tarikkhadri11@gmail.com>
2024-03-20 01:58:16 +0000

    f

--- console.py
+++ console.py
@@ -156,7 +156,7 @@ class HBNBCommand(cmd.Cmd):
             if len(class_name) == 0:
                 print("** class name missing **")
                 return
-            if class_name and class_name not in self.valid_classes:
+            if class_name and class_name not in HBNBCommand.classes:
                 print("** class doesn't exist **")
                 return
 
