252430d3ce0cb0ba8d28f75109629be127fbcfc4
OUMA13 <omaimaelkhanchoufi9@gmail.com>
2024-03-20 18:31:19 -0700

    init.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,13 @@
+#!/usr/bin/python3
 
+   """Instantiates a storage object"""
+
+from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
+from os import getenv
+
+if getenv("HBNB_TYPE_STORAGE") == "db":
+    storage_ot = DBStorage()
+else:
+    storage_ot = FileStorage()
+storage_ot.reload()
