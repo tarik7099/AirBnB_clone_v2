0d069c6b93a3b281b2df90041d2df5b95a9e76c7
OUMA13 <omaimaelkhanchoufi9@gmail.com>
2024-03-21 15:43:23 -0700

    state

--- models/state.py
+++ models/state.py
@@ -1,26 +1,34 @@
 #!/usr/bin/python3
-
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
-from models.base_model import BaseModel, Base
-from os import getenv
+""" State Module for HBNB project """
 import models
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+from os import getenv
+# from dotenv import load_dotenv
+
+# This line brings all environment variables from .env into os.environ
+# load_dotenv()
+
 
 class State(BaseModel, Base):
-    """State class"""
-
-    __tablename__ = "states"
-
-    name = Column(String(128), nullable=False)
+    """ State class """
 
     if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = "states"
+
+        name = Column(String(128), nullable=False)
         cities = relationship('City', backref='state', cascade='all, delete')
     else:
+        name = ""
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
-            city_list = []
-            for city in list(models.storage.all("City").values()):
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
-
+            """
+            getter attribute cities that returns the list of City instances
+            with state_id equals to the current State.id => It will be the
+            FileStorage relationship between State and City
+            """
+            return [city for city in models.storage.all("City").values()
+                    if city.state_id == self.id]
