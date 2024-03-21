15d7af43ee9c1ddff53827834c59aedebf1e6f49
tarikkhadri <tarikkhadri11@gmail.com>
2024-03-21 01:46:17 +0000

    da

--- models/city.py
+++ models/city.py
@@ -14,11 +14,10 @@ class City(BaseModel, Base):
     """
 
     if getenv("HBNB_TYPE_STORAGE") == "db":
-        __tblnm__ = 'cities'
-
-        name_o = Column(String(128), nullable=False)
-        state_id_o = Column(String(60), ForeignKey('states.id'), nullable=False)
-        plcs_o = relationship('Place', backref='cities', cascade='all, delete')
+        __tablename__ = "cities"
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+        places = relationship("Place", backref="cities", cascade="delete")
 
     else:
         name = ""
