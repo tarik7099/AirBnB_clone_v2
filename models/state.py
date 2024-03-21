4e148936d5665b6d25be987ecc2691c68e3746a6
OUMA13 <omaimaelkhanchoufi9@gmail.com>
2024-03-21 15:36:50 -0700

    state

--- models/state.py
+++ models/state.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-        
-""" State Module for HBNB project """
-from sqlalchemy import Column, String, ForeignKey, DateTime
+
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 from os import getenv
@@ -10,22 +9,18 @@ import models
 class State(BaseModel, Base):
     """State class"""
 
+    __tablename__ = "states"
+
+    name = Column(String(128), nullable=False)
+
     if getenv("HBNB_TYPE_STORAGE") == "db":
-        __tblname__ = "states"
-
-        name_ot = Column(String(128), nullable=False)
-        cities_ot = relationship('City', backref='state', cascade='all, delete')
+        cities = relationship('City', backref='state', cascade='all, delete')
     else:
-        name_ot = ""
-
-    if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
-            """
-           Getter attribute for cities
-            """
-            city_list_ot = []
-            for city_ot in list(models.storage.all("City").values()):
-                if city_ot.state_id == self.id:
-                    city_list_ot.append(city_ot)
-            return city_list_ot
+            city_list = []
+            for city in list(models.storage.all("City").values()):
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
+
