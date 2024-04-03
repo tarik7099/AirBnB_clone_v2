f3ba4e8a98d4c501c88412c0d71d7d7e7fc63a05
TARIK KHADRI <tarikkhadri112gmail.com>
2024-04-03 20:15:39 +0000

    task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -13,4 +13,6 @@ echo "<html>
 </html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
+grep -q "location /hbnb_static" /etc/nginx/sites-available/default || \
+sed -i "/^}/i \ \n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t\tautoindex off;\n\t}" /etc/nginx/sites-available/default
 service nginx restart
