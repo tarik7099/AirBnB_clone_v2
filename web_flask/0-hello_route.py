08faa793482bf2fde2dde96ec10cc16ad03291e9
tarik khadri <tarikkhadri11@gmail.com>
2024-04-19 22:36:48 +0100

    foix

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,6 +10,7 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """return hello"""
     return 'Hello HBNB!'
 
 
